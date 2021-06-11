25d2b0616c0d0cfd2fe8b8e16943a0b26e94b0ff
TariqMBooker <55111113+TariqMBooker@users.noreply.github.com>
2021-06-11 10:48:38 -0400

    Update OptionMenu.java

--- OptionMenu.java
+++ OptionMenu.java
@@ -2,3 +2,18 @@ package io.github.vikeshpandey.corejava.project.atm;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class OptionMenu extends Account {
+  Scanner menuInput = new Scanner(System.in);
+  DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
+  
+  HashMap<Integer, Integer> data = new HashMap<Integer, Integer>();
+  
+  public void getLogin() throws IOException {
+    int x = 1;
+    do {
+      try {
+        data.put(952141, 191904);
+        data.put(989947, 71976);
